0727298af80157b64ef0963731aa2c91f7bff8be
Hind <hind.nayhi@gmail.com>
2022-07-27 08:55:58 -0600

    Modified the assertArraysEqual function to make it more functional.

--- flatten.js
+++ flatten.js
@@ -15,7 +15,9 @@ const assertArraysEqual = function(arr1, arr2) {
   if (!eqArrays(arr1, arr2)) { //Print a msg depending on if eqArrays is true or false.
     console.log(`🛑 Assertion Failed: ${arr1} !== ${arr2}`);
   }
+  if (eqArrays(arr1, arr2)) {
   console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  }
 };
 
 const flatten = function(arr) {
@@ -34,3 +36,4 @@ const flatten = function(arr) {
 };
 
 assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 6]);
