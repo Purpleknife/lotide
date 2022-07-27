94154de85e3b50a28f98cf2bc117f7c745c1309b
Hind <hind.nayhi@gmail.com>
2022-07-27 10:14:12 -0600

    Created a function countLetters to count the letters in a string and return it as an object.

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  let count = {};
+
+  for (letter of str) {
+    if (letter !== ' ') {
+      count[letter] = count.hasOwnProperty(letter) 
+        ? count[letter] + 1 
+        : 1;
+    }
+  }
+  return count;
+};
+
+console.log(countLetters("lighthouse in the house"));
+console.log(countLetters('LHL'));
+
+assertEqual(countLetters("lighthouse in the house"), { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 });
+assertEqual(countLetters('LHL'), { L: 2, H: 1 });
