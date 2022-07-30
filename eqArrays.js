703fd19025c1d6507f3547de11671701fab7572a
Hind <hind.nayhi@gmail.com>
2022-07-29 18:29:58 -0600

    Checked eslint and fixed errors.

--- eqArrays.js
+++ eqArrays.js
@@ -12,7 +12,7 @@ const eqArrays = function(arr1, arr2) { //Compare both arrays and if they're a p
   if (arr1.length !== arr2.length) { //Compare length first, if it doesn't match, exit function and return false.
     return false;
   }
-  for (let i = 0; i < arr1.length; i++) { //Compare each element one by one if length matches. 
+  for (let i = 0; i < arr1.length; i++) { //Compare each element one by one if length matches.
     if (arr1[i] !== arr2[i] && (!Array.isArray(arr1[i]) || !Array.isArray(arr2[i]))) {
       return false;
     }
@@ -20,7 +20,7 @@ const eqArrays = function(arr1, arr2) { //Compare both arrays and if they're a p
       if (!eqArrays(arr1[i], arr2[i])) {
         return false;
       }
-    } 
+    }
   }
   return true; //If length and all the elements are a perfect match, return true.
 };
